199928cdbc25ecacc3b30445c820d83d5f913f07
RithwikK17 <rithwik2603@gmail.com>
2025-06-18 11:43:19 +0530

    reactHooks

--- hooks/src/Util/todos.js
+++ hooks/src/Util/todos.js
@@ -10,3 +10,23 @@ export function createTodos(){
     }
     return todos;
 }
+
+// filter todos
+export function filterTodos(todos,tab){
+    console.log(`[Artificially slow] filtering ${todos.length} todos for ${tab} tab...`);
+
+    let startTime = performance.now()
+    while(performance.now() - startTime < 500){
+        // do nothing for 500ms
+    }
+
+    return todos.filter(val => {
+        if(tab === "all"){
+            return true; // all todos
+        }else if(tab === "active") {
+            return !val.completed; // false
+        } else if(tab === "completed"){
+            return val.completed; // true
+        }
+    })
+}
