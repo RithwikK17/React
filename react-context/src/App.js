778b460728d175c0f5797acf241ffc4a7ceae1f5
RithwikK17 <rithwik2603@gmail.com>
2025-06-25 11:29:08 +0530

    React Context

---
+++ react-context/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
+import Menu from './Components/Menu'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Menu/>
+      App
+    </BrowserRouter>
+  )
+}
+
+export default App
